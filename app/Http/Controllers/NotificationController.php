556a076952c4b03ae8da08456aee4789cec4f1fa
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2022-02-25 12:37:49 +0100

    kjsd

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -7,37 +7,28 @@ use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
-    public function index(Request $rq){
-
+    public function index(Request $rq)
+    {
         $user = $rq->user();
-
         $notifications = $user->startup->startupNotifications()->get();
-        
-        return[
-            "data"=>$notifications,
-            "statut"=>200
-        ];
 
+        return [
+            "data" => $notifications,
+            "statut" => 200
+        ];
     }
 
-    
-
-
-    public function notificationRead($id){
-
-
+    public function notificationRead($id)
+    {
         $notification = StartupNotifications::find($id);
         $notification->viewed = true;
         $notification->save();
 
-
-        return[
-            "msg"=>"statut de la notification modifié avec succés",
-            "statut"=>200
+        return [
+            "msg" => "statut de la notification modifié avec succés",
+            "statut" => 200
         ];
-
     }
 
-
-
+    
 }
